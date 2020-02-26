2ff0d06a28bb603990f2ac6ae6496510ee637da5
kerttujaansalu <kerttu.jaansalu@khk.ee>
2020-02-26 15:15:11 +0200

    document.links omadused

--- app.js
+++ app.js
@@ -20,6 +20,12 @@ sisu = document.contentType;
 sisu = document.forms; // kõik vormid
 sisu = document.forms[0]; // esimene vorm
 sisu = document.forms[0].id; // esimese vormi id
-sisu = document.forms[0].method; // vormi töötlus meetod
+sisu = document.forms[0].method; // vormi töötlusmeetod
 sisu = document.forms[0].action;
+
+// linkide andmed
+sisu = document.links; // kõik lingid
+sisu = document.links[0]; // esimene link
+sisu = document.links[0].className; // lingi klassid
+sisu = document.links[0].classList[1]; // klasside massiiv ja sealt elemendi lugemine
 console.log(sisu); 
