2f47c4f801ca612c34467ca40ce23651106eaedb
kerttujaansalu <kerttu.jaansalu@khk.ee>
2020-02-27 00:04:11 +0200

    Objekti peototüübi täiendamine - perenime vahetus

--- app.js
+++ app.js
@@ -18,9 +18,15 @@ Isik.prototype.arvutaVanus = function(){
   const taisAasta = vanusDateKujul.getUTCFullYear();
   const vanus = taisAasta - 1970;
   return vanus;
-    
   }
+
+  // abiellumine
+Isik.prototype.abiellus = function(uusPereNimi){
+    this.perenimi = uusPereNimi;
+  }
+
   const kerttu = new Isik('Kerttu', 'Jaansalu', '02-18-1999');
   const rudolf = new Isik('Rudolf', 'Saluoks', '05-23-1999');
   console.log(kerttu);
+  kerttu.abiellus('Saluoks');
   console.log(rudolf.taisNimi()); 
