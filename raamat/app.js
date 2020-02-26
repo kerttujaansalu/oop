735403904a6f87a3001d950714eab553b4fb7549
kerttujaansalu <kerttu.jaansalu@khk.ee>
2020-02-27 01:15:21 +0200

    Raamatud - raamatute salvestamine LS-sse

--- raamat/app.js
+++ raamat/app.js
@@ -65,6 +65,22 @@ KL.prototype.kustutaRaamatTabelist = function(kustutaElement){
    }, 5000);
 }
 
+// raamatu salvestamine LS-sse
+KL.prototype.salvestaRaamat = function(r){
+    // loome raamatute hoidla LS-s
+    let raamatud;
+    // kui raamatud veel LS-s ei eksisteeri
+    if(localStorage.getItem('raamatud') === null){
+      raamatud = [];
+    } else {
+      // kui aga raamatud juba olemas, saame need kätte
+      raamatud = JSON.parse(localStorage.getItem('raamatud'));
+    }
+    raamatud.push(r);
+    localStorage.setItem('raamatud', JSON.stringify(raamatud));
+    console.log(raamatud);
+  }
+
   // kirjeldame raamatu lisamise sündmust
 document.getElementById('book-form').addEventListener('submit', lisaRaamat);
 // raamatu lisamise funktsioon
@@ -87,6 +103,8 @@ function lisaRaamat(e){
     // muidu
     // lisame sisestatud raamat tabelisse
     kl.lisaRaamatTabelisse(raamat);
+    // salvestame raamatu andmed LS-sse
+    kl.salvestaRaamat(raamat);
     kl.teade('Raamat on lisatud!', 'valid');
   }
 
