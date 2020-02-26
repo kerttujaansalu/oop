fcf8c1a49a5b1108cb490133b6d427e830c2374b
kerttujaansalu <kerttu.jaansalu@khk.ee>
2020-02-27 00:44:48 +0200

    Raamatud - loodud raamat vormi andmetest

---
+++ raamat/app.js
@@ -0,0 +1,22 @@
+// Raamatu konstruktor
+function Raamat(a, p, i){
+    this.autor = a;
+    this.pealkiri = p;
+    this.isbn = i;
+  }
+
+  // kirjeldame raamatu lisamise sündmust
+document.getElementById('book-form').addEventListener('submit', lisaRaamat);
+// raamatu lisamise funktsioon
+function lisaRaamat(e){
+  // võtame andmed vormist
+  const pealkiri = document.getElementById('title').value;
+  const autor = document.getElementById('author').value;
+  const isbn = document.getElementById('isbn').value;
+  // loome raamat andmete põhjal
+  const raamat = new Raamat(pealkiri, autor, isbn);
+
+  console.log(raamat);
+
+  e.preventDefault();
+} 
