3fe1439af77a2adf787003e8b1221c2ecd0c685e
kerttujaansalu <kerttu.jaansalu@khk.ee>
2020-02-26 23:32:50 +0200

    LocalStorage - andmete kirjutamine ja lugemine

--- app.js
+++ app.js
@@ -1,28 +1,43 @@
-// sündmused
+// storage
 
-// bubbling
+// salvestame väärtus
+// localStorage.setItem('eesnimi', 'Kerttu');
+// localStorage.setItem('perenimi', 'Jaansalu');
 
-// lehe kaardi pealkiri
-// document.querySelector('.card-title').addEventListener('click', function(e){
-//   console.log('card-title');
-// });
+// väärtuse eemaldamine
+// localStorage.removeItem('nimi');
 
-// lehe kaardi sisu
-document.querySelector('.card-content').addEventListener('click', function(e){
-    console.log('card-content');
-  });
-  
-  // delegeerimine
-  
-  const kustutaYlesanne = document.querySelector('.delete-item');
-  kustutaYlesanne.addEventListener('click', kustuta);
-  
-  // console.log(kustutaYlesanne);
-  
-  function kustuta(e){
-     if(e.target.parentElement.classList.contains('delete-item')){
-      console.log('hakkame kustutama');
-      e.target.parentElement.parentElement.remove();
-    } 
+// väärtuste lugemine
+// const eesnimi = localStorage.getItem('eesnimi');
+// console.log(eesnimi);
+// const perenimi = localStorage.getItem('perenimi');
+// console.log(perenimi);
+
+// eemalda kõik väärtused
+// localStorage.clear();
+
+document.querySelector('form').addEventListener('submit', salvesta);
+
+function salvesta(e){
+  const uusYlesanne = document.getElementById('task').value;
+  let ylesanded;
+  if(localStorage.getItem('tasks') === null){
+    ylesanded = []; // tekitame massiivi, kui veel ülesandeid pole
+  } else {
+    ylesanded = JSON.parse(localStorage.getItem('tasks'));
+    // loeme LS andmed, teisendame JSON elemendiks - JS struktuuriks
   }
-  
+  console.log(ylesanded);
+  ylesanded.push(uusYlesanne); // lisame element massiivi lõppu
+  console.log('ulesanne lisatud');
+  localStorage.setItem('tasks', JSON.stringify(ylesanded)); // lisame andmed LS sisse
+  e.preventDefault();
+}
+
+
+
+ // loeme sisestatud ülesanded
+ const ylesanded = JSON.parse(localStorage.getItem('tasks'));
+ ylesanded.forEach(ylesanne => {
+   console.log(ylesanne);
+ });
