bc8cff6e136dd08b0a9c06d7bc5352aab06a1a66
kerttujaansalu <kerttu.jaansalu@khk.ee>
2020-02-26 15:11:06 +0200

    document.forms omadused

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ sisu = document.all; // html tagid kasutamisjärjekorras
 sisu = document.all[2]; // konkreetne html tag
 sisu = document.all.length; // palju on html tagi kasutusel
 
-// document properties - oobjekti omadused
+// document properties - objekti omadused
 sisu = document.head; // head sisu
 sisu = document.body; // body sisu
 sisu = document.doctype; // doctype
@@ -15,4 +15,11 @@ sisu = document.domain;
 sisu = document.URL;
 sisu = document.characterSet;
 sisu = document.contentType;
+
+// formi andmed
+sisu = document.forms; // kõik vormid
+sisu = document.forms[0]; // esimene vorm
+sisu = document.forms[0].id; // esimese vormi id
+sisu = document.forms[0].method; // vormi töötlus meetod
+sisu = document.forms[0].action;
 console.log(sisu); 
