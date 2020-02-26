ffc8ec91b633d6f6d36d175b4f7473d2a7869f35
kerttujaansalu <kerttu.jaansalu@khk.ee>
2020-02-27 00:49:48 +0200

    Raamatud - raamatu lisamine tabelisse

--- raamat/app.js
+++ raamat/app.js
@@ -19,6 +19,20 @@ KL.prototype.puhastaSisend = function(){
   document.getElementById('isbn').value = '';
 }
 
+// raamatu lisamine tabelisse
+KL.prototype.lisaRaamatTabelisse = function(r){
+    // loome tabeli rida
+    const rida = document.createElement('tr');
+    // täidame rida tabeli andmetega
+    rida.innerHTML = `
+      <td>${r.pealkiri}</td>
+      <td>${r.autor}</td>
+      <td>${r.isbn}</td>
+    `;
+    // lisame rida tabelisse
+    tabel = document.getElementById('book-list');
+    tabel.appendChild(rida);
+  }
 
   // kirjeldame raamatu lisamise sündmust
 document.getElementById('book-form').addEventListener('submit', lisaRaamat);
@@ -28,13 +42,14 @@ function lisaRaamat(e){
   const pealkiri = document.getElementById('title').value;
   const autor = document.getElementById('author').value;
   const isbn = document.getElementById('isbn').value;
-  // loome raamat andmete põhjal
-  const raamat = new Raamat(pealkiri, autor, isbn);
-
-  console.log(raamat);
+  // loome raamatu andmete põhjal
+  const raamat = new Raamat(autor, pealkiri, isbn);
 
   // loome kasutajaliidese objekt temaga opereerimiseks
   const kl = new KL();
+
+  // lisame sisestatud raamatu tabelisse
+  kl.lisaRaamatTabelisse(raamat);
 
   // puhastame väljad sisestatud andmetest
   kl.puhastaSisend();
